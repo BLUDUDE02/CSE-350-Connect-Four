a6eed051166478166c702ae1d9d3c96e88801a8c
aprmayyyjun <107215624+aprmayyyjun@users.noreply.github.com>
2022-07-24 15:11:28 -0400

    initial commit
    added mult files to separate new classes

---
+++ GameBoard.py
@@ -0,0 +1,57 @@
+import numpy as np
+import pygame
+
+P1Token = 1
+P2Token = 2
+height = 700
+
+class GameBoard:
+    def _init_(self, RowCount, ColumnCount, SquareSize, screen):
+        self.RowCount = RowCount
+        self.ColumnCount = ColumnCount
+        self.SquareSize = SquareSize
+        self.screen = screen
+        
+    def CreateBoard(self):
+        RowCount = self.RowCount
+        ColumnCount = self.ColumnCount
+        board = np.zeros((RowCount, ColumnCount))
+        return board
+    
+    def DrawBoard(self):
+        SquareSize = self.SquareSize
+        RowCount = self.RowCount
+        ColumnCount = self.ColumnCount
+        screen = self.screen
+        
+        casing = pygame.image.load('Casing.png')
+        casing.convert()
+        rect = casing.get_rect()
+        blankspace = pygame.image.load('Blank-Space.png')
+        blankspace.convert()
+        rect0 = blankspace.get_rect()
+        redtok = pygame.image.load('Red-Token.png')
+        redtok.convert()
+        rect1 = redtok.get_rect()
+        yeltok = pygame.image.load('Yellow-Token.png')
+        yeltok.convert()
+        rect2 = yeltok.get_rect()
+        
+        print("Building Board!")
+        
+        for c in range(ColumnCount):
+            for r in range(RowCount):
+                rect.center = (int(c*SquareSize+SquareSize/2), int(r*SquareSize+SquareSize+SquareSize/2))
+                rect0.center = (int(c*SquareSize+SquareSize/2), int(r*SquareSize+SquareSize+SquareSize/2))
+                screen.blit(casing, rect)
+                screen.blit(blankspace, rect0)
+        for c in range(ColumnCount):
+            for r in range(RowCount):        
+                if self[r][c] == P1Token:
+                    rect1.center = (int(c*SquareSize+SquareSize/2), height-int(r*SquareSize+SquareSize/2))
+                    screen.blit(redtok, rect1)
+                elif self[r][c] == P2Token: 
+                    rect2.center = (int(c*SquareSize+SquareSize/2), height-int(r*SquareSize+SquareSize/2))
+                    screen.blit(yeltok, rect2)
+        pygame.display.update()
+
---
+++ main.py
@@ -0,0 +1,94 @@
+import pygame
+import pygame_menu
+import os.path as path
+import os
+
+
+
+BACKGROUND = path.join(path.dirname(path.abspath(__file__)), '{0}').format('Background.png')
+
+background_image = pygame_menu.BaseImage(
+    image_path=BACKGROUND)
+
+           
+def background() -> None:
+    """
+    Function used by menus, draw on background while menu is active.
+    
+
+    background = pygame.image.load('Background.png')
+    background.convert()
+    
+    background_image = pygame.image.load('Background.png')
+    surface.blit(background_image)
+    
+    pygame.display.update()
+    """
+    global screen
+    background_image.draw(screen)
+
+def main(test: bool = False) -> None:
+    
+    global main_menu
+    global options_menu
+    global screen
+    global width
+    global height
+    
+    pygame.init()
+    
+    #Post PyGame Initialization Variable Declarations    
+    width = 700
+    height = 700
+    size = (width, height)
+    screen = pygame.display.set_mode(size)
+    
+    
+    # -------------------------------------------------------------------------
+    # Menus
+    # -------------------------------------------------------------------------
+    theme = pygame_menu.themes.THEME_DARK.copy()
+    theme.title_font = pygame_menu.font.FONT_8BIT
+    theme.widget_font = pygame_menu.font.FONT_8BIT
+    theme.background_color = (0, 0, 0, 180)
+    
+    main_menu = pygame_menu.Menu('Connect 4', width, height, theme=theme)
+    main_menu.add.button('Two Player', StartGame, False)
+    main_menu.add.button('Player VS AI', StartGame, True)
+    main_menu.add.button('Exit', pygame_menu.events.EXIT)
+    
+    options_menu = pygame_menu.Menu('Game Over', 400, 300, theme=theme)
+    options_menu.add.button('Two Player', StartGame, False)
+    options_menu.add.button('Player VS AI', StartGame, True)
+    options_menu.add.button('Exit', pygame_menu.events.EXIT)
+    
+    
+    while True:
+        
+        # events handling for menus -- turn this in to function
+        events = pygame.event.get()
+        for e in events:
+            if e.type == pygame.QUIT:
+                os._exit(00)
+        
+        # Pass events to main_menu
+        if main_menu.is_enabled():
+            main_menu.update(events)
+            
+        if options_menu.is_enabled():
+            options_menu.update(events)
+            options_menu.draw(screen)
+    
+        # Main menu
+        if main_menu.is_enabled():
+            main_menu.mainloop(screen, background)
+            
+        # Flip surface
+        pygame.display.flip()
+
+        # At first loop returns
+        if test:
+            break
+        
+if __name__ == '__main__':
+        main()
